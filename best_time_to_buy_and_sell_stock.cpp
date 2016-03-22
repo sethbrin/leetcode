951ba92cce9adc271c27bf427b013f690424873a
sethbrin <zengping10212044@gmail.com>
2016-03-23 00:08:54 +0800

    use o(n) time and o(1) to calc the problem

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -2,21 +2,18 @@ class Solution {
 public:
 
     int maxProfit(vector<int>& prices) {
-        int len = prices.size();
-        if (len <= 1) {
-            return 0;
-        }
-        vector<int> dp(len, 0);
-        // dp[i] indicate that we sell stock at day i, we can get the maximum profit
-        dp[0] = 0;
-        dp[1] = prices[1] - prices[0];
-        for (int i=2; i<len; i++) {
-            int tmp = prices[i] - prices[i-1];
-            dp[i] = dp[i-1] < 0?tmp:(dp[i-1] + tmp);
-        }
+      int len = prices.size();
+      if (len <= 1) {
+        return 0;
+      }
 
-        return *max_element(dp.begin(), dp.end());
-
+      int buy = prices[0];
+      int sell = 0;
+      for (int i=1; i<len; i++) {
+        sell = max(sell, prices[i] - buy);
+        buy = min(buy, prices[i]);
+      }
+      return sell; 
     }
 
 };
