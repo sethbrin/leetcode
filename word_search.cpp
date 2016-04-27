3cfcd5f4c28b1ac718a9a5ae77edbc56d7366229
sethbrin <zengping10212044@gmail.com>
2016-04-27 22:53:58 +0800

    use visited, 56.51%

--- word_search.cpp
+++ word_search.cpp
@@ -1,36 +1,34 @@
 class Solution {
-public:
-    // find if in the area (0,0) -> (row,col) contains the sub of the word
-    bool exist(vector<vector<char>>& board, int row, int col, string word, int ind) {
-        if (ind == word.size()) {
-            return true;
-        }
-        
-        if (row < 0 || row >= board.size() || col < 0 || col >= board[0].size() || board[row][col] != word[ind]) {
-            return false;
-        }
-        
-        // set board[row][col] to a unque char
-        board[row][col] = 3;
-        
-        bool res = exist(board, row-1, col, word, ind+1) ||
-               exist(board, row+1, col, word, ind+1) ||
-               exist(board, row, col+1, word, ind+1) ||
-               exist(board, row, col-1, word, ind+1);
-        board[row][col] = word[ind];
+private:
+
+    bool dfs(const vector<vector<char>>& board, const string& word, int index, int x, int y, vector<vector<bool>>& visited) {
+        if (index == word.size()) return true;
+        if (x < 0 || y < 0 || x >= board.size() || y >= board[0].size()) return false;
+        if (board[x][y] != word[index] || visited[x][y]) return false;
+
+        visited[x][y] = true;
+        bool res =  dfs(board, word, index+1, x-1, y, visited) // top
+            || dfs(board, word, index+1, x+1, y, visited) // bottom
+            || dfs(board, word, index+1, x, y-1, visited) // left
+            || dfs(board, word, index+1, x, y+1, visited); // right;
+
+        visited[x][y] = false;
         return res;
-        
     }
+public:
     bool exist(vector<vector<char>>& board, string word) {
-        // use recursion
-        
-        for (int i=0; i<board.size(); i++) {
-            for (int j=0; j<board[0].size(); j++) {
-                if (exist(board, i, j, word, 0)) {
-                    return true;
-                }
+        int rows = board.size();
+        if (rows == 0) return false;
+        int cols = board[0].size();
+        if (cols == 0) return false;
+
+        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
+        for (int i=0; i<rows; i++) {
+            for (int j=0; j<cols; j++) {
+                if (dfs(board, word, 0, i, j, visited)) return true;
             }
         }
+
         return false;
     }
 };
