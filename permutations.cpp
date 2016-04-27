ae9fae93fe7fea936272684ac5f898d6c026c1df
sethbrin <zengping10212044@gmail.com>
2016-04-27 23:52:11 +0800

    46. Permutations custom next_permutation 72.27%

--- permutations.cpp
+++ permutations.cpp
@@ -1,16 +1,43 @@
 class Solution {
 
+    typedef vector<int>::iterator IntIterator;
+private:
+    bool next_permutation1(IntIterator start, IntIterator end) {
+        if (start == end) return false;
+        IntIterator i = end;
+        i --;
+        if (start == i) return false; // only one elements
+
+        while (true) {
+            IntIterator j = i;
+            i -- ;
+
+            // if the before element is less then later one
+            if (*i < *j) {
+                j = end;
+                while (!(*i < *--j)) {}
+                iter_swap(i, j);
+                reverse(i+1, end); // reverse from j to the end
+                return true;
+            }
+
+            if (i == start) {
+                return false;
+            }
+        }
+    }
+
 public:
     vector<vector<int>> permute(vector<int>& nums) {
         vector<vector<int>> res;
+        
         sort(nums.begin(), nums.end());
 
         do{
             res.push_back(nums);
-        } while(next_permutation(nums.begin(), nums.end()));
+        } while(next_permutation1(nums.begin(), nums.end()));
 
         return res;
 
     }
-
 };
