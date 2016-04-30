b085d247f4ed6e5327ec4e7a49f03a2a8b23ef29
sethbrin <zengping10212044@gmail.com>
2016-04-30 10:52:35 +0800

    use one dimension dp

--- unique_path2.cpp
+++ unique_path2.cpp
@@ -5,20 +5,16 @@ public:
         if (rows == 0) return 0;
         int cols = obstacleGrid[0].size();
         
-        vector<vector<int>> dp(rows, vector<int>(cols, 0));
-        dp[0][0] = 1;
+        vector<int> dp(cols, 0);
+        dp[0] = 1;
         for (int i=0; i<rows; i++) {
             for (int j=0; j<cols; j++) {
-                if (obstacleGrid[i][j] == 1) {
-                    dp[i][j] = 0;
-                } else {
-                    if (i == 0 && j == 0) continue;
-                    else if (i == 0) dp[i][j] = dp[i][j-1];
-                    else if (j == 0) dp[i][j] = dp[i-1][j];
-                    else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+                if (obstacleGrid[i][j]) dp[j] = 0;
+                else if(j > 0) {
+                    dp[j] += dp[j-1];
                 }
             }
         }
-        return dp[rows-1][cols-1];
+        return dp[cols-1];
     }
 };
